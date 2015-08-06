06eff9ca996f8ddd6769f8c5e1c79233e432285f
suzukisan22 <getty2017@gmail.com>
2015-08-06 16:50:09 +0000

    before want-have homework

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ActiveRecord::Base
 
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  
+  has_many :wamts  , foreign_key: "item_id" , dependent: :destroy
+  has_many :want_users , through: :wants , source: :user
+  
+  has_many :haves  , foreign_key: "item_id" , dependent: :destroy
+  has_many :have_users , through: :haved , source: :user
+  
 end
